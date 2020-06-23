ab74a0ce158a121b5ff2a0fcd1c5a5478310df00
mr-srini <mr-srini@users.noreply.github.com>
2020-06-23 11:27:56 +0530

    DAY 2

--- lib/userpages/userstart.dart
+++ lib/userpages/userstart.dart
@@ -6,12 +6,33 @@ class UserStartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.center,
         // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+              height: 450,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    buttonColor,
+                  ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage("images/logainpage.jpg"),
+                ),
+              ),
+            ),
+          ),
           FlatButton(
-            onPressed: (){
-
+            onPressed: () {
+              print("checkin");
             },
             child: Container(
               margin: EdgeInsets.all(5),
@@ -50,5 +71,23 @@ class UserStartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
