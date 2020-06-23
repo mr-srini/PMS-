ba4f792c2ce92551fece0d5b99b81353ace42866
mr-srini <mr-srini@users.noreply.github.com>
2020-06-23 10:56:23 +0530

    DAY 2

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color accentColor = Color(0xFF1376FF);
+const Color textColor = Colors.white;
+const Color buttonColor = Color(0xff5c5bb0);
+const double letterspacing = 5.0;
+const double fontSize = 20.0;
+
